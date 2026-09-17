97db23e635b9c0b575ea165739a0fd0787476b8d
iamsujanbudhathoki <iamsujanbudhathoki@gmail.com>
2026-09-17 13:45:28 +0545

    fix(admin): fix modal overlay backdrop, de-emphasize header/sidebar and restrict background interactivity

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -31,7 +31,7 @@ function DialogOverlay({
     <DialogPrimitive.Backdrop
       data-slot="dialog-overlay"
       className={cn(
-        "fixed top-header-height inset-x-0 bottom-0 isolate z-modal-overlay bg-black/10 duration-100 supports-backdrop-filter:backdrop-blur-xs data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0",
+        "fixed inset-0 isolate z-modal-overlay bg-slate-900/50 backdrop-blur-xs duration-100 data-open:animate-in data-open:fade-in-0 data-closed:animate-out data-closed:fade-out-0",
         className
       )}
       {...props}
@@ -55,7 +55,7 @@ function DialogContent({
       <DialogPrimitive.Popup
         data-slot="dialog-content"
         className={cn(
-          "fixed top-modal-top left-1/2 z-modal-content flex flex-col w-full max-w-[calc(100%-2rem)] max-h-modal-max-h overflow-y-auto -translate-x-1/2 -translate-y-1/2 rounded-xl bg-popover text-sm text-popover-foreground ring-1 ring-foreground/10 duration-100 outline-none sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
+          "fixed top-1/2 left-1/2 z-modal-content flex flex-col w-full max-w-[calc(100%-2rem)] max-h-modal-max-h overflow-y-auto -translate-x-1/2 -translate-y-1/2 rounded-xl bg-popover text-sm text-popover-foreground ring-1 ring-foreground/10 duration-100 outline-none sm:max-w-sm data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95",
           className
         )}
         {...props}
